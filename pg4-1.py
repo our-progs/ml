414985c281ad5771c1b2505a34acb88e1936598b
our-progs <mrspectacled.yt@gmail.com>
2025-05-27 22:04:28 +0530

    Update pg4-1.py

--- pg4-1.py
+++ pg4-1.py
@@ -8,18 +8,7 @@ data = {
 'Covid-19':['positive','positive','negative','positive','negative'] 
 }  
 
-
-# data = {
-#     "Sky": ["Sunny", "Sunny", "Rainy", "Sunny"],
-#     "AirTemp":["Normal", "High", "High", "High"],
-#     "Wind": ["Strong", "Strong", "Strong", "Strong"],
-#     "Water": ["Warm", "Warm", "Warm", "Cold"],
-#     "Forcast": ["Same", "Same", "Change", "Change"],
-#     "EnjoySport": ["Yes", "Yes", "No", "Yes"]
-# }
-
 df = pd.DataFrame(data) 
 
 df.to_csv('covid.csv', index=False) 
-# df.to_csv('sport.csv', index=False) 
 print("CSV file created successfully!")
