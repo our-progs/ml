f17f45f74641c85dcfcb330b3bb04b1c73b8ec84
our-progs <mrspectacled.yt@gmail.com>
2025-05-27 22:05:20 +0530

    Update pg4-2.py

--- pg4-2.py
+++ pg4-2.py
@@ -1,11 +1,9 @@
 import csv  
 a = [ ]   
-with open('covid.csv', 'r') as csvfile: 
-# with open('sport.csv', 'r') as csvfile:      
+with open('covid.csv', 'r') as csvfile:     
     for row in csv.reader(csvfile):         
         a.append(row) 
-        print(row)     
-    # print(a, "\n")  
+        print(row)       
 print ("\n The total number of training inwstances are : ", len(a)-1)   
 
 num_attribute = len(a[0])-1  
@@ -14,8 +12,7 @@ hypothesis = ['0'] * num_attribute
 print(hypothesis)   
 
 for i in range(1, len(a)):     
-    if a[i][num_attribute] == 'positive':   
-    # if a[i][num_attribute] == 'Yes':       
+    if a[i][num_attribute] == 'positive':         
         for j in range(0, num_attribute):              
             if hypothesis[j] == '0' or hypothesis[j] == a[i][j]:                  
                 hypothesis[j] = a[i][j]              
